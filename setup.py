21061bcc07e4d817d46612640861a381ba5ad7b8
vangheem <vangheem@gmail.com>
2018-10-20 22:44:44 -0400

    Back to development: 1.0.4

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except IOError:
 
 setup(
     name='guillotina_authentication',
-    version='1.0.3',
+    version='1.0.4.dev0',
     description='Authenticate Guillotina with various providers',
     long_description=README,
     install_requires=[
