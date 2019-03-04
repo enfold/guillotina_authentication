c26f2014bd3abda9d48bcc860c5672fa2dd31d90
Ramon Navarro Bosch <ramon.nb@gmail.com>
2019-03-04 10:45:04 +0100

    Back to development: 1.0.7

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except IOError:
 
 setup(
     name='guillotina_authentication',
-    version='1.0.6',
+    version='1.0.7.dev0',
     description='Authenticate Guillotina with various providers',
     long_description=README,
     install_requires=[
